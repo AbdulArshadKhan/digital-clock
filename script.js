b3e252d6097a899224c4c5a75d15e3bcc0a598d2
Arshad <khanarshadb1163@gmail.com>
2022-06-04 17:53:34 +0530

    Digital Clock Using Javascript
    Learning Points:-
    
      1. Padding the value of minutes and seconds to form the required format
    
      2. Removing the delay of showing time after reload by calling the updateTime() function directly

---
+++ script.js
@@ -0,0 +1,46 @@
+const clockElement = document.querySelector(".clock")
+
+function getTimeParts()
+{
+    const now = new Date();
+
+    let hour = now.getHours();
+
+    if(hour>12) hour = hour % 12;
+    
+    const minute = now.getMinutes();
+
+    const seconds = now.getSeconds()
+
+    const isAm = now.getHours() < 12 ;
+
+    return{
+        hour,minute,seconds,isAm
+    };
+}
+
+function updateTime()
+{
+    let parts = getTimeParts();
+    let formttedMinute,formattedSecond;
+    
+    formttedMinute = parts.minute.toString().padStart(2,'0')
+        
+    formattedSecond = parts.seconds.toString().padStart(2,"0")
+    
+    const amPm = parts.isAm?'AM':'PM';
+    
+    const time = `${parts.hour}:${formttedMinute}:${formattedSecond}`
+
+    document.querySelector('.clock-time').textContent = time;
+
+    document.querySelector('.clock-ampm').textContent = amPm
+    
+}
+
+updateTime()
+
+setInterval(()=>{
+    updateTime()
+
+},500)
